a9549f6440884a5563788085a3f38a8c9f2bb06c
Seth Hall <seth.m.hall07@gmail.com>
2026-08-25 12:03:59 -0500

    New Commit

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,7 +1,15 @@
+//We love testing new things
+
+/*multi line comments
+to see how
+this works alltogether*/
+
 public class HelloWorld
 {
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+        System.out.println("Doing things to practice");
     }
 }
